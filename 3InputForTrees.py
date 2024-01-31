884f4b6eb1ac7ab8118c554d2d2377a130ff6f54
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-01-30 16:26:54 -0800

    Added code to take dynamic tree input

--- 3InputForTrees.py
+++ 3InputForTrees.py
@@ -1,1 +1,48 @@
 
+class BinaryTreeNode:
+    
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+        
+def printTree(root):
+    
+    #base case if root is None return nothing
+    
+    if root is None:
+        return
+    
+    print(root.data, end=":")
+    
+    if root.left is not None:
+        print("L => ", root.left.data, end=',')
+    
+    if root.right is not None:
+        print("R => ", root.right.data, end= ",")
+    print()
+    
+    printTree(root.left) #to access our left Subtree
+    printTree(root.right) #to access our right subtree
+    
+
+def inputTree():
+    root = int(input())
+    
+    if root == -1:
+        return None
+    
+    rootNode = BinaryTreeNode(root)
+    leftTree = inputTree()
+    rightTree = inputTree()
+    
+    #here we are building bottom to top
+    #all subtrees in the bottom will get connect to main trees going up
+    rootNode.left = leftTree
+    rootNode.right = rightTree
+    
+    return rootNode
+
+root = inputTree()
+printTree(root)
