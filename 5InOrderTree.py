11e9eb5146c631117ec9f1d37ff258fc83185671
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-03-13 21:47:37 -0700

    Pushed In Order Traversal Code

--- 5InOrderTree.py
+++ 5InOrderTree.py
@@ -1,1 +1,35 @@
+class BinaryTreeNode:
+    
+    def __init__(self,data):
+        self.data = data
+        self.left = None 
+        self.right = None
+        
+        
+#Making A basic Tree HArdcoded
 
+n1 = BinaryTreeNode(10)
+n2 = BinaryTreeNode(5)
+n3 = BinaryTreeNode(20)
+n4 = BinaryTreeNode(3)
+n5 = BinaryTreeNode(75)
+
+n1.left = n2
+n1.right = n3
+
+n2.left = n4
+n3.right = n5
+
+def printInOrder(root): #Left Root Right
+    
+    if root is None:
+        return
+    
+    
+    printPostOrder(root.left)
+    print(root.data)
+    printPostOrder(root.right)
+    
+
+
+printInOrder(n1)
